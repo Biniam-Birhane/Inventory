181a204255c67d2f243d75bcedfe5489e41bcb35
biniam <biniam043@gmail.com>
2024-04-21 09:59:44 +0300

    update

--- lib/dashboard/drawer.dart
+++ lib/dashboard/drawer.dart
@@ -6,124 +6,133 @@ import "package:simple_inventory/products/presentation/pages/products.dart";
 Drawer DrawerPage(Size size, BuildContext context) {
   return Drawer(
     backgroundColor: Color(0xFF151D26),
-    child: Padding(
-      padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 0),
-      child: Container(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      child: ListTile(
-                        horizontalTitleGap: size.width * 0.06,
-                        leading: const Icon(Icons.shopping_cart,
-                            color: Colors.white),
-                        title: const Text(
-                          "Add Products",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "Quicksand",
-                            fontSize: 15,
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Products()));
-                        },
-                      ),
-                    ),
-                    Divider(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      height: size.height * 0.01,
-                    ),
-                    Container(
-                      child: ListTile(
-                        horizontalTitleGap: size.width * 0.06,
-                        leading:
-                            const Icon(Icons.category, color: Colors.white),
-                        title: const Text(
-                          "Product Categories ",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "Quicksand",
-                            fontSize: 15,
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ProductCategory()));
-                        },
-                      ),
-                    ),
-                    Divider(
-                      height: 10,
-                    ),
-                    Container(
-                      child: ListTile(
-                        horizontalTitleGap: size.width * 0.06,
-                        leading: const Icon(
-                          Icons.person,
+    child: Container(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 30.0, 30.0, 10.0),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    backgroundColor: Color(0xFF151D26),
+                    foregroundImage: AssetImage('assets/images/logo.png'),
+                    radius: 30,
+                  ),
+                  SizedBox(
+                    width: size.width * 0.06,
+                  ),
+                  const Text(
+                    "Board Inventory",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: 'Quicksand'),
+                  ),
+                ],
+              )),
+          const Divider(
+            height: 10,
+            color: Colors.white,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Container(
+              child: Column(
+                children: [
+                  Container(
+                    child: ListTile(
+                      horizontalTitleGap: size.width * 0.06,
+                      leading:
+                          const Icon(Icons.shopping_cart, color: Colors.white),
+                      title: const Text(
+                        "Add Products",
+                        style: TextStyle(
                           color: Colors.white,
+                          fontFamily: 'Quicksand',
+                          fontSize: 18,
                         ),
-                        title: const Text(
-                          "Customers",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "Quicksand",
-                            fontSize: 15,
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Customers()));
-                        },
                       ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Products()));
+                      },
                     ),
-                    const Divider(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      height: size.height * 0.01,
-                    ),
-                    Container(
-                      child: ListTile(
-                        horizontalTitleGap: size.width * 0.06,
-                        leading: const Icon(
-                          Icons.payment,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.01,
+                  ),
+                  Container(
+                    child: ListTile(
+                      horizontalTitleGap: size.width * 0.06,
+                      leading: const Icon(Icons.category, color: Colors.white),
+                      title: const Text(
+                        "Product Categories ",
+                        style: TextStyle(
                           color: Colors.white,
+                          fontFamily: "Quicksand",
+                          fontSize: 18,
                         ),
-                        title: const Text(
-                          "Payment",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "Quicksand",
-                            fontSize: 15,
-                          ),
-                        ),
-                        onTap: () {},
                       ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductCategory()));
+                      },
                     ),
-                    Divider(
-                      height: 10,
+                  ),
+                  Container(
+                    child: ListTile(
+                      horizontalTitleGap: size.width * 0.06,
+                      leading: const Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      title: const Text(
+                        "Customers",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Quicksand",
+                          fontSize: 18,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Customers()));
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.01,
+                  ),
+                  Container(
+                    child: ListTile(
+                      horizontalTitleGap: size.width * 0.06,
+                      leading: const Icon(
+                        Icons.payment,
+                        color: Colors.white,
+                      ),
+                      title: const Text(
+                        "Payment",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Quicksand",
+                          fontSize: 18,
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     ),
   );
